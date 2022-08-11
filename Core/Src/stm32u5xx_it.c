9b5d3bbbc2fbd257ab59ce463e28d7e5b6cbc46f
Frank-G5 <zifangzhao@gmail.com>
2022-08-11 16:34:55 -0400

    test_with_I sweep

--- Core/Src/stm32u5xx_it.c
+++ Core/Src/stm32u5xx_it.c
@@ -237,8 +237,11 @@ void TIM16_IRQHandler(void)
 	//HAL_ADC_Start_IT(&hadc1);
 	LL_ADC_REG_StartConversion(hadc1.Instance);
 	//AD5522_SetOutputCurrent_float(&h_PMU,PMU_CH_0|PMU_CH_1,((float)ADC_temp[0]-32768)/65535.0*3.3/1000.0);
+
+	AD5522_SetOutputVoltage_float(&h_PMU,PMU_CH_0|PMU_CH_1,get_waveform(0));
+	//AD5522_SetOutputCurrent_float(&h_PMU,PMU_CH_0|PMU_CH_1,get_waveform(0)/10000);
 	
-	AD5522_SetOutputCurrent_float(&h_PMU,PMU_CH_0|PMU_CH_1,get_waveform(0)/1000);
+	
   /* USER CODE END TIM16_IRQn 0 */
   /* USER CODE BEGIN TIM16_IRQn 1 */
 
